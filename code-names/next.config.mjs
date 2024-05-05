bb23c6e9b7c67ba70f23b01040e25114b9432498
Jared <hamjared5@gmail.com>
2024-05-05 07:57:47 -0600

    Initial Commit

---
+++ code-names/next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/CodeNames",
+    assetPrefix: "/CodeNames",
+    output: "export",
+    reactStrictMode: true,
+};
+
+export default nextConfig;
